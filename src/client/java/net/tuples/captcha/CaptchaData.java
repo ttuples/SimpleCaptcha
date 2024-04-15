ce4878388a9c1daa3022ea03559391fc4930b8b9
Paul Cameron <kd8ogdtfrespect@gmail.com>
2024-04-14 20:11:09 -0400

    - Removed unused/commented code

--- src/client/java/net/tuples/captcha/CaptchaData.java
+++ src/client/java/net/tuples/captcha/CaptchaData.java
@@ -36,36 +36,4 @@ public class CaptchaData {
             throw new RuntimeException(e);
         }
     }
-
-//    private static void parseJsonData() {
-//        try {
-//            JsonObject json = getJsonObject();
-//
-//            for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
-//                String id = entry.getKey();
-//                JsonObject data = entry.getValue().getAsJsonObject();
-//
-//                String name = data.get("text").getAsString();
-//                boolean creep = data.get("creep").getAsBoolean();
-//
-//                // Skip adding creepy captchas if disabled in config
-//                if (creep && !Captcha.config.enableCreepyCaptchas)
-//                    continue;
-//
-//                Captcha.LOGGER.info("Loading captcha {}", id);
-//
-//                captchas.add(name, new JsonObject());
-//
-//                JsonObject imageData = data.get("images").getAsJsonObject();
-//
-//                for (Map.Entry<String, JsonElement> data_entry : imageData.entrySet()) {
-//                    String path = data_entry.getKey();
-//                    Boolean dataValue = data_entry.getValue().getAsBoolean();
-//                    captchas.
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
