394fa68fabed35930ddb52d35efd428b23dd09d1
Tuples <93516054+ttuples@users.noreply.github.com>
2025-09-05 16:17:58 -0400

    fix neoforge modid

--- common/src/main/java/net/tuples/captcha/ExampleMod.java
+++ common/src/main/java/net/tuples/captcha/ExampleMod.java
@@ -1,7 +1,7 @@
 package net.tuples.captcha;
 
 public final class ExampleMod {
-    public static final String MOD_ID = "simple-captcha";
+    public static final String MOD_ID = "captcha";
 
     public static void init() {
         // Write common init code here.
